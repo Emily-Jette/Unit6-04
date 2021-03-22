9f63a8403cf8d9878b66f15886cbe97b9f489bd8
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-22 13:36:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = 0
+let b = 0
+let height = 0
+let area = 0
+
+document.getElementById('button').addEventListener('click', calculate)
+
+function calculate () {
+  a = document.getElementById('baseA').value
+  a = parseInt(a)
+  b = document.getElementById('baseB').value
+  b = parseInt(b)
+  height = document.getElementById('height').value
+  height = parseInt(height)
+  area = ((a+b)/2)*height
+  document.getElementById('answer').innerHTML = area
+}
+
